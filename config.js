779d78510e423a1e39bb0e167b442a0beba274c3
winter477 <jasperloneria29@gmail.com>
2025-08-07 16:31:05 +0800

    config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "muisc bot", 
+  activityName: "muisc bot powerd by winter dev", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xPwKemrQ7k",
   embedTimeout: 5, 
