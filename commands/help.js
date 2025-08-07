cbfb5ab72044da67f8afe29c6da7b872db7b1e39
winter477 <jasperloneria29@gmail.com>
2025-08-07 21:57:56 +0800

    help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ const musicIcons = require('../UI/icons/musicicons.js');
 
 module.exports = {
   name: "help",
-  description: "Get information about the bot for dev",
+  description: "Get information about the bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction, lang) => {
